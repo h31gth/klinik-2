9f7aeb7c01e2923f2946da2e0a4eb55e5965b9cb
daffa03x <mobadaff@gmail.com>
2022-07-07 20:58:16 +0700

    use API covid

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -11,9 +11,19 @@
           <div class="col-md-5 col-xl-6 col-xxl-7 order-0 order-md-1 text-end"><img class="pt-7 pt-md-0 w-100"
               src="{!! asset('assets/img/home.png') !!}" alt="hero-header" /></div>
           <div class="col-md-75 col-xl-6 col-xxl-5 text-md-start text-center py-6">
-            <h1 class="fw-light font-base fs-6 fs-xxl-7">Sistem Informasi <strong>Klinik
+            <h1 class="fw-light font-base fs-6 fs-xxl-8">Sistem Informasi <strong>Klinik
               </strong></h1>
-            <p class="fs-1 mb-3">Memberikan kemudahan dalam pendaftaran pasien, melihat antrian, melihat jadwal dokter dan melihat poliklinik yang ada di klinik ini.</p><a class="btn btn-lg btn-primary rounded-pill" href="{{ url('landingpage/pendaftaran') }}" role="button">Lakukan Pendaftaran</a>
+              <h3>Informasi COVID 19 Kuningan</h3>
+              @foreach ($covid as $item)
+                  <div>Total Terkonfirmasi : {{ $item['total_terkonfirmasi'] }} orang</div>
+              @endforeach
+              @foreach ($covid as $item)
+              <div>Total Sembuh : {{ $item['total_sembuh'] }} orang</div>
+          @endforeach
+          @foreach ($covid as $item)
+          <div>Total Kematian : {{ $item['total_meninggal'] }} orang</div>
+      @endforeach
+              <a class="btn btn-lg btn-primary rounded-pill mt-2" href="{{ url('landingpage/pendaftaran') }}" role="button">Lakukan Pendaftaran</a>
           </div>
         </div>
     </section>
@@ -43,21 +53,19 @@
         <div class="row align-items-center">
           <div class="col-md-6 order-lg-1 mb-5 mb-lg-0"><img class="fit-cover rounded-circle w-100" src="{!! asset('images/ilustrasi/ilustration2.png') !!}" alt="..." /></div>
           <div class="col-md-6 text-center text-md-start">
-            <h2 class="fw-bold mb-4">Kami mengembangkan sistem pelayanan
-               <br class="d-none d-sm-block" />kesehatan untuk Anda</h2>
-            <p>Kami berpikir bahwa setiap orang harus memiliki akses <br class="d-none d-sm-block" />mudah ke perawatan kesehatan yang sangat baik. Tujuan kami <br class="d-none d-sm-block" />adalah membuat prosedur sesederhana mungkin bagi pasien kami dan <br class="d-none d-sm-block" />menawarkan perawatan di mana pun mereka berada — secara langsung atau sesuai kenyamanan mereka. </p>
+            <h2 class="fw-bold mb-4">Aplikasi Pendaftaran Antrian Di klinik</h2>
+            <p>Memberikan kemudahan dalam pendaftaran pasien, melihat antrian, melihat jadwal dokter dan melihat poliklinik yang ada di klinik ini.</p>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="py-5">
-
+    <section class="py-xxl-10 pb-0" id="home">
+      <div class="bg-holder bg-size" style="background-image:url(assets/img/gallery/dot-bg.png);background-position:top left;background-size:auto;">
+      </div>
       <div class="container">
         <div class="row">
           <div class="col-12 pt-3">
-            <div class="bg-holder bg-size" style="background-image:url{{ asset('assets/img/gallery/blog-post.png') }};background-position:top center;background-size:contain;">
-            </div>
             <!--/.bg-holder-->
             <h1 class="text-center">POLIKLINIK</h1>
           </div>
@@ -70,16 +78,15 @@
     <!-- ============================================-->
 
 
-    <section>
-      <div class="bg-holder bg-size" style="background-image:url(assets/img/gallery/dot-bg.png);background-position:top left;background-size:auto;">
-      </div>
+
+    <section class="py-xxl-10 pb-0" id="home">
       <!--/.bg-holder-->
 
       <div class="container">
-        <div class="row">
+        <div class="row h-100 m-lg-7 mx-3 mt-6 mx-md-4 my-md-7">
           @foreach ($data as $item)
-          <div class="col-sm-6 col-lg-3 mb-4">
-            <div class="card h-100 shadow card-span rounded-3"><img class="card-img-top rounded-top-3 img-thumbnail" src="{{ asset($item->image) }}" style="max-width: 260px; max-height:150px" alt="news" />
+          <div class="col-md-4 mb-md-5">
+            <div class="card h-100 shadow card-span rounded-3"><img class="card-img-top rounded-top-3 img-thumbnail" src="{{ asset($item->image) }}" style="max-width: 280px; max-height:150px" alt="news" />
               <div class="card-body">
                 <h5 class="font-base fs-lg-0 fs-xl-1 mt-2">{{ $item->nama }}</h5>
                 <div class="div">{{ $item->deskripsi }}</div>
@@ -110,7 +117,7 @@
                       </div>
                       @endforeach
                     </div>
-          @include('landingpage.partials.banner')
         </div>
       </section>
+      @include('landingpage.partials.banner')
       @endsection
